a7a5464d82e8d75b2ba9c4424213f3343c38faf6
aleena <aleenamariya97@gmail.com>
2021-10-18 19:25:22 +0530

    hello

--- src/main/java/com/obsqura/HelloWorld.java
+++ src/main/java/com/obsqura/HelloWorld.java
@@ -2,7 +2,7 @@ package com.obsqura;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 6; i++) {
             int k=i+1;
             System.out.println(k+2);
 System.out.println("hello");
