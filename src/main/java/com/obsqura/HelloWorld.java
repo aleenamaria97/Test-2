6494b65aba8bfca502c57c89604b1ca7899c7dbf
unknown <aleenamariya97@gmail.com>
2021-10-18 08:35:35 +0530

    first commit

---
+++ src/main/java/com/obsqura/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.obsqura;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            int k=i+1;
+            System.out.println(k);
+
+        }
+    }
+}
