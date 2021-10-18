80e2c74490c0574fdc6fa48d87cac16b010d42b7
aleena <aleenamariya97@gmail.com>
2021-10-18 22:39:52 +0530

    first commit

---
+++ src/main/java/com/obsqura/New.java
@@ -0,0 +1,16 @@
+package com.obsqura;
+
+public class New {
+
+
+        public static void main(String[] args) {
+            for (int i = 0; i < 6; i++) {
+                int m;
+                m = i;
+                System.out.println(m);
+                System.out.println("hello");
+            }
+        }
+    }
+
+
